eaf79648dfa795504525f1f2eb1424033ce9998a
fasmoreira <felippe10augusto@gmail.com>
2020-03-04 19:01:24 -0300

    update

---
+++ Aplicacao/ICM/ICM/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){});
+
+
+/*
+	A VARIAVEL "saldoDeMoedas" SERVIRÁ PARA AS PAGINAS:
+	Index.aspx, Resgate.aspx, Extrato.aspx
+*/
+let saldoDeMoedas = 220; // VALOR QUE VEM DO BANCO
